9c8fa33c79ff6a86a167c46275e5b9cadf52af6d
AlcorBBX <lev.matisov55@gmail.com>
2022-04-22 22:53:53 +0500

    master

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -40,25 +40,29 @@ const Login = observer(() => {
         }
 
     }
+    // color: "#9c27b0"
+
     return(
         <div className='Login'>
-            <Card variant="outlined" className='Log' sx={{ maxWidth: 400, p: 5 }}  style={{backgroundColor:'#f6d365'}}>
-                <Typography variant="h6">{ isLogin ? 'Авторизация' : 'Регистрация'}</Typography>
-                
+            <Card variant="outlined" className='Log' sx={{ maxWidth: 400, p: 5 }}  style={{background:'none', border:'none', textAlign: 'center'}}>
+                <Typography variant="h6" 
+                style={{color: "#eee", fontWeight: '200', fontSize: '1.6em', margin: '0', pading: '0' }}>{ isLogin ? 'Авторизация' : 'Регистрация'}</Typography>
                 <form className='form'>
-                    <TextField style={{marginBottom: "30px", marginTop: "15px"}}
+                    <TextField className="loginInp" 
                     value={email} 
                     onChange = {e => setEmail(e.target.value)}
-                    type="text" placeholder="" label="Email" />
+                    type="text" placeholder="" label="Email" 
+                        style={{border: '0px', marginBottom: '20px'}}
+                    />
                     <TextField  
                     value={password} 
                     onChange = {e => setPassword(e.target.value)} 
                     type="password" placeholder="" label="Пароль" />
-                    { isLogin ? <div>Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрироваться</NavLink></div>
-                    : <div className = "w-auto">Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войти</NavLink></div>}
+                    { isLogin ? <div>Нет аккаунта? <NavLink style={{color: "#9c27b0", fontWeight: '200'}} to={REGISTRATION_ROUTE}>Зарегестрироваться</NavLink></div>
+                    : <div className = "w-auto">Есть аккаунт? <NavLink style={{color: "#9c27b0", fontWeight: '200'}} to={LOGIN_ROUTE}>Войти</NavLink></div>}
                     <Button 
                      onClick={click} 
-                    variant="contained" sx={{ mt: 3 }}>{isLogin ? 'Войти' : 'Зарегестрироваться'}</Button>
+                    variant="outlined" sx={{ mt: 3 }}>{isLogin ? 'Войти' : 'Зарегестрироваться'}</Button>
 
                 </form>
 
